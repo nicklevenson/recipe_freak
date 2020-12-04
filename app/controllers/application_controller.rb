edf50c5143f273a0849f0068c4887026021289b2
Nicholas Levenson <nicklevenson@gmail.com>
2020-12-04 10:45:57 -0800

    added signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'random_string'
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,31 @@
 class UsersController < ApplicationController
 
+  get "/users/:id" do
+    @user = User.find(params[:id])
+    erb :"/users/show.html"
+  end
+
   get "/signup" do
     erb :"/users/new.html"
   end
 
   post "/signup" do
+    user = User.new(params[:user])
+    if User.exists?(username: user.username) || User.exists?(email: user.email) 
+      @error = "Username or email already exists" 
+      erb :'/users/new.html'
     
+    else
+      if user.save 
+        session[:user_id] = user.id
+        redirect "/users/#{user.id}"
+        
+      else 
+        @error = user.errors.full_messages.join(" - ")
+        erb :'/users/new.html'
+      end
+    end
+  
   end
 
 end
