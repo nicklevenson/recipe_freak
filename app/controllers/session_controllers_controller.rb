9dc6b5a6ca9265bcf40c5b7f63529777c3bc47ab
Nicholas Levenson <nicklevenson@gmail.com>
2020-12-04 11:50:03 -0800

    signin

--- app/controllers/session_controllers_controller.rb
+++ app/controllers/session_controllers_controller.rb
@@ -5,7 +5,15 @@ class SessionControllersController < ApplicationController
   end
 
   post '/login' do
+    user = User.find_by(username: params[:username])
 
+    if user && user.authenticate(params[:password])
+      session[user_id] = user.id
+      redirect '/users/home'
+    else
+      @error = "Incorrect username or password"
+      erb :'sessions/new.html'
+    end
   end
 
   delete '/logout' do 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,10 @@ class UsersController < ApplicationController
   get "/users/home" do
    
     if session[:user_id]
-      @user = User.find(session[:user_id])
       erb :"/users/show.html"
     else
       @error = "Please log in or sign up to view this content"
-      erb :'welcome'
+      erb :"/users/show.html"
     end
   end
 
@@ -17,21 +16,13 @@ class UsersController < ApplicationController
 
   post "/signup" do
     user = User.new(params[:user])
-    if User.exists?(username: user.username) || User.exists?(email: user.email) 
-      @error = "Username or email already exists" 
-      erb :'/users/new.html'
-    
-    else
-      if user.save 
-        session[:user_id] = user.id
-        redirect "/users/home"
+    if user.save 
+      session[:user_id] = user.id
+      redirect "/users/home"
         
-      else 
-        @error = user.errors.full_messages.join(" - ")
-        erb :'/users/new.html'
-      end
+    else 
+      @error = user.errors.full_messages.join(" - ")
+      erb :'/users/new.html'
     end
-  
   end
-
 end
