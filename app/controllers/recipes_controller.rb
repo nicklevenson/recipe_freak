314ed9fc718c8251a2841553ac42ea651ac81b7d
Nicholas Levenson <nicklevenson@gmail.com>
2020-12-05 12:33:01 -0800

    edit able

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -42,7 +42,16 @@ class RecipesController < ApplicationController
 
   # PATCH: /recipes/5
   patch "/recipes/:id" do
-    redirect "/recipes/:id"
+    recipe = Recipe.find(params[:id])
+    recipe.update(params[:recipe])
+    Cuisine.find_or_create_by(params[:cuisine]).recipes << recipe
+
+    recipe.ingredients.destroy_all
+    recipe.ingredients << Ingredient.create(params[:ingredient])
+
+    recipe.steps.destroy_all
+    recipe.steps << Step.create(params[:step])
+    redirect "/recipes/#{recipe.id}"
   end
 
   # DELETE: /recipes/5/delete
