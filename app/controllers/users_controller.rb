cff9ad175f0ecef2c92f5175bac83541383d6f7d
Nicholas Levenson <nicklevenson@gmail.com>
2020-12-06 12:17:24 -0800

    likes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,4 +26,29 @@ class UsersController < ApplicationController
       erb :'/users/new.html'
     end
   end
+
+  post "/users/like" do
+    if logged_in? 
+      user = User.find(session[:user_id])
+      recipe = Recipe.find(params[:recipe_id])
+      if (user.likes.select{|like|like.recipe.id == recipe.id}) == []
+        like = Like.new
+        recipe.likes << like
+        user.likes << like
+        redirect '/'
+      end
+    else
+      @recipes = Recipe.all
+      @like_error = "Sign in or sign up to like recipes!"
+      erb :welcome
+    end
+  end
+
+  delete '/users/like' do
+    user = User.find(session[:user_id])
+    recipe = Recipe.find(params[:recipe_id])
+    like = Like.find_by(user_id: user.id, recipe_id: recipe.id)
+    like.destroy
+    redirect '/'
+  end
 end
