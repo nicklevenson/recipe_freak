b0174a39d5450d2089ecf295e4c363de3fdfd00d
Nicholas Levenson <nicklevenson@gmail.com>
2020-12-05 17:12:38 -0800

    added faker seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@ Cuisine.create({name: "Italian"}).recipes << recipe
 recipe.ingredients << Ingredient.create({name: "noodles", amount: 1, unit_type: "Cup"})
 
 recipe.steps << Step.create({action: "Make the spaget"})
+
+10.times do
+  user1 = User.create(email: Faker::Internet.unique.email , username:  Faker::Name.unique.name , password: Faker::Color.unique.hex_color)
+  recipe1 = Recipe.create(name: Faker::Food.dish)
+  user1.recipes << recipe1
+  Cuisine.create({name: Faker::Restaurant.type}).recipes << recipe1
+  3.times do
+  recipe1.ingredients << Ingredient.create({name: Faker::Food.ingredient, amount: rand(10), unit_type: Faker::Food.metric_measurement})
+  recipe1.steps << Step.create({action: "Do the thing"})
+  end
+end
