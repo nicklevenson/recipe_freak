776531f71d2f7c6a5c0afcdc1803c3a819e401b9
Nicholas Levenson <nicklevenson@gmail.com>
2020-12-09 11:55:19 -0800

    added ingredient method

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,8 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
+
+  def step_change(new_amount)
+    ratio = self.recipe.serving_size.to_f / new_amount
+    self.amount * ratio
+  end
 end
