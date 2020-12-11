f452b56f197c7d16408266a1ec2eb6e830fdb81a
Nicholas Levenson <nicklevenson@gmail.com>
2020-12-10 20:54:45 -0800

    pg?

--- config/environment.rb
+++ config/environment.rb
@@ -6,13 +6,21 @@ Bundler.require
 
 require_all 'app'
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
 
-configure :development do 
-  set :database, 'sqlite3:db/development.sqlite'
+# configure :development do 
+#   set :database, 'sqlite3:db/development.sqlite'
+# end
+
+configure :development do
+  set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'recipe_freak_db', pool: 2, username: 'nicklevenson', password: 'Circle26'}
+end
+
+configure :production do
+  set :database, {adapter: 'postgresql',  encoding: 'unicode', database: 'recipe_freak_db', pool: 2, username: 'nicklevenson', password: 'Circle26'}
 end
 
 use Rack::MethodOverride
