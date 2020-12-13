198f249f97ab1192d35a924e0cfcad6c54706e85
Nicholas Levenson <nicklevenson@gmail.com>
2020-12-13 11:54:21 -0800

    filter only public cuisines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,21 +19,21 @@ class ApplicationController < Sinatra::Base
     if params[:sort] == "recent"
       @last = "Recent-Old"
       @recipes = Recipe.all
-      @cuisines = Recipe.all.collect{|recipe|recipe.cuisine.name}.uniq
+      @cuisines = Recipe.all.select{|recipe| recipe.public? == true}.collect{|recipe|recipe.cuisine.name}.uniq
       erb :welcome
     elsif params[:sort] == "popular"
       @recipes = Recipe.all.sort_by{|recipe| recipe.likes.count}
-      @cuisines = Recipe.all.collect{|recipe|recipe.cuisine.name}.uniq
+      @cuisines = Recipe.all.select{|recipe| recipe.public? == true}.collect{|recipe|recipe.cuisine.name}.uniq
       @last = "Popular"
       erb :welcome
     elsif Cuisine.all.collect{|cuisine| cuisine.name}.include?(params[:sort])
       @last = params[:sort]
       @recipes = Recipe.all.select{|recipe| recipe.cuisine.name == params[:sort]}
-      @cuisines = Recipe.all.collect{|recipe|recipe.cuisine.name}.uniq
+      @cuisines = Recipe.all.select{|recipe| recipe.public? == true}.collect{|recipe|recipe.cuisine.name}.uniq
       erb :welcome
     elsif params[:sort] == "name"
       @last = "Alphabetical"
-      @cuisines = Recipe.all.collect{|recipe|recipe.cuisine.name}.uniq
+      @cuisines = Recipe.all.select{|recipe| recipe.public? == true}.collect{|recipe|recipe.cuisine.name}.uniq
       @recipes = Recipe.all.sort_by{|recipe| recipe.name}.reverse
       erb :welcome
     elsif params[:sort][:keyword] 
@@ -42,7 +42,7 @@ class ApplicationController < Sinatra::Base
       by_ingredients = Ingredient.all.select{|ingredient| ingredient.name.downcase.include?(params[:sort][:keyword].downcase)}.collect{|ingredient| ingredient.recipe}.select{|recipe|recipe.public? == true}
       by_username = Recipe.all.select{|recipe| recipe.user.username.downcase.include?(params[:sort][:keyword].downcase)}.select{|recipe|recipe.public? == true}
       @recipes = (by_name + by_cuisine + by_ingredients + by_username).uniq
-      @cuisines = Recipe.all.collect{|recipe|recipe.cuisine.name}.uniq
+      @cuisines = Recipe.all.select{|recipe| recipe.public? == true}.collect{|recipe|recipe.cuisine.name}.uniq
       erb :welcome
     end
 
